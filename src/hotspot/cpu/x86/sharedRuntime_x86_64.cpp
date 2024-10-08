c8b3001fd37955dd84cbe97dec6ee342c5b997d2
Ashutosh Mehra <asmehra@redhat.com>
2024-10-08 14:31:30 -0400

    Save/restore adapter handlers
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- src/hotspot/cpu/x86/sharedRuntime_x86_64.cpp
+++ src/hotspot/cpu/x86/sharedRuntime_x86_64.cpp
@@ -28,6 +28,7 @@
 #endif
 #include "asm/macroAssembler.hpp"
 #include "asm/macroAssembler.inline.hpp"
+#include "cds/cdsConfig.hpp"
 #include "code/SCCache.hpp"
 #include "code/compiledIC.hpp"
 #include "code/debugInfoRec.hpp"
@@ -1056,26 +1057,26 @@ void SharedRuntime::gen_i2c_adapter(MacroAssembler *masm,
 }
 
 // ---------------------------------------------------------------
-AdapterHandlerEntry* SharedRuntime::generate_i2c2i_adapters(MacroAssembler *masm,
-                                                            int total_args_passed,
-                                                            int comp_args_on_stack,
-                                                            const BasicType *sig_bt,
-                                                            const VMRegPair *regs,
-                                                            AdapterFingerPrint* fingerprint) {
+void SharedRuntime::generate_i2c2i_adapters(MacroAssembler *masm,
+                                            int total_args_passed,
+                                            int comp_args_on_stack,
+                                            const BasicType *sig_bt,
+                                            const VMRegPair *regs,
+                                            AdapterHandlerEntry* entry) {
   address i2c_entry = __ pc();
-  const char* name = AdapterHandlerLibrary::name(fingerprint);
-  const uint32_t id = AdapterHandlerLibrary::id(fingerprint);
+  const char* name = AdapterHandlerLibrary::name(entry->fingerprint());
+  const uint32_t id = AdapterHandlerLibrary::id(entry->fingerprint());
   // we need to avoid restoring code that might refer to stub routines
   // until we know they are available likewise we don't want to save
   // early generated adapters that try to refernce them
-  bool attempt_save_restore = (StubRoutines::final_stubs_code() != nullptr);
-  if (attempt_save_restore) {
+  if (true) {
     // see if we can load the code rather than generate it
     CodeBuffer* buffer = masm->code();
     uint32_t offsets[4];
     if (SCCache::load_adapter(buffer, id, name, offsets)) {
       assert(offsets[0] == 0, "sanity check");
-      return AdapterHandlerLibrary::new_entry(fingerprint, i2c_entry, i2c_entry + offsets[1], i2c_entry + offsets[2], i2c_entry + offsets[3]);
+      entry->set_entry_points(i2c_entry, i2c_entry + offsets[1], i2c_entry + offsets[2], i2c_entry + offsets[3]);
+      return;
     }
   }
   gen_i2c_adapter(masm, total_args_passed, comp_args_on_stack, sig_bt, regs);
@@ -1137,7 +1138,7 @@ AdapterHandlerEntry* SharedRuntime::generate_i2c2i_adapters(MacroAssembler *masm
 
   gen_c2i_adapter(masm, total_args_passed, comp_args_on_stack, sig_bt, regs, skip_fixup);
 
-  if (attempt_save_restore) {
+  if (CDSConfig::is_dumping_adapters()) {
     // try to save generated code
     CodeBuffer* buffer = masm->code();
     uint32_t offsets[4];
@@ -1147,7 +1148,7 @@ AdapterHandlerEntry* SharedRuntime::generate_i2c2i_adapters(MacroAssembler *masm
     offsets[3] = c2i_no_clinit_check_entry - i2c_entry;
     SCCache::store_adapter(buffer, id, name, offsets);
   }
-  return AdapterHandlerLibrary::new_entry(fingerprint, i2c_entry, c2i_entry, c2i_unverified_entry, c2i_no_clinit_check_entry);
+  entry->set_entry_points(i2c_entry, c2i_entry, c2i_unverified_entry, c2i_no_clinit_check_entry);
 }
 
 int SharedRuntime::c_calling_convention(const BasicType *sig_bt,
